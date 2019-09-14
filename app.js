a39a31725d840ff7c710b43d49db4a919b627426
Pratham Dogra <dogra.pratham@gmail.com>
2019-09-14 22:36:59 +0530

    Created basic GraphQl API

--- app.js
+++ app.js
@@ -3,19 +3,33 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const app = express();
-
+const events=[];
 app.use(bodyParser.json());
 
 app.use(
   "/api",
   graphqlHttp({
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price:Float!
+        date:String!
+    }
     type RootQuery {
-        events:[String!]!
+        events:[Event!]!
+    }
+    input EventInput{
+        title:String!
+        description: String!
+        price:Float!
+        date: String!
     }
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
+    
     schema {
         query: RootQuery
         mutation: RootMutation
@@ -23,15 +37,22 @@ app.use(
      `),
     // the rootValue is the resolvers in graphQL
     rootValue: {
-        events: ()=>{
-            return ['Robotics', 'Dance', 'Drama', 'Coding', 'Cooking'];
-        },
-        createEvent:(arg)=>{
-            const eventName = arg.name;
-            return eventName;
-        }
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+          const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+          };
+          events.push(event);
+          return event;
+              }
     },
-    graphiql:true
+    graphiql: true
   })
 );
 const port = 5000 || process.env.PORT;
